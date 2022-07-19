1c7aea9ef3b82a4675d1ae0d6e04fc8148caedc9
poojan411 <poojanp411@gmail.com>
2022-07-19 11:06:02 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          vasve website project setup initial commit
         </p>
         <a
           className="App-link"
