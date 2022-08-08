67679b3e99d9e5cfc4347e0dc935436c407efb57
Poojan <poojanp411@gmail.com>
2022-08-08 16:42:12 +0100

    Added staticData for Services

---
+++ src/config/staticData.jsx
@@ -0,0 +1,255 @@
+export const virtuesAndValues = [
+    {
+        title: 'INTEGRITY',
+        description: 'Here at Vasave, integrity is found in everything that we do and produce as we have solid ethical and moral principles in which all our employees follow at all times. We act with honesty, honour and truthfulness even if no one is watching. We believe that by firmly believing and following this, a client will be more likely to do business with us and attract the right people to work for us, resulting in the retention of good customers allowing us to develop our business even more.',
+        imageUrl: ""
+    },
+    {
+        title: 'LEADERSHIP',
+        description: 'As the great Alexandre Havard would say, "Leadership is about achieving greatness and bringing out greatness in others." At Vasave, a quote has never been closer to home; all the employees within this organisation are pushed to their limits, reaching their full potential and be the best version of themselves that they can be by providing them with the most suitable training and experience that will allow them to be themselves and flourish in their expertise.By developing employees talents, this organisation has proven time and time again that we are able to outperform competitors and excel where others do not',
+        imageUrl: ""
+    },
+    {
+        title: 'RESPONSIBILITY',
+        description: 'We believe that this is one of those virtues that has a broad meaning. For starters, this could mean having the responsibilities to its customers and employees by delivering on its promises; on the other hand, it could also mean considering the impact its operations may have on society and the environment. Here at Vasave, everything is considered in everything we do and produce; everything matters to us: the environment, customers, employers, and our stakeholders.',
+        imageUrl: ""
+    },
+    {
+        title: 'FAIRNESS',
+        description: 'At Vasave, we believe that people will remain loyal to your business by interacting fairly with your customers, employers and everyone else involving your organization. It means managing all employees and customers with dignity, respect and not using standards for one group of individuals that make others feel as if they are not as valued, favoured or essential. To ensure that we treat everyone around us fairly, we make your compensation structure transparent and give everyone an equal opportunity to be recognized, promoted, and appeal decisions they consider unfair. Fairness is a virtue that has served businesses well for decades, and this is something we stay true to.',
+        imageUrl: ""
+    },
+    {
+        title: 'CONSISTENCY',
+        description: 'Here at Vasave, we believe that a company that is consistent with firmly applying its values in everything that they do and produce will have a solid corporate culture and structure. Customers and employees want to know that the business will be constant in terms of the performance and the quality of work produced, whatever the task is in hand, whether it would be when attempting to increase the sales of a product when promoting or creating content for the product. Demonstrating values consistently will reassure the companys stakeholders, helping to build a solid and growing organization.',
+        imageUrl: ""
+    },
+
+
+  ];
+  export const ourGoals = [
+    {
+        title: 'HIRING POTENTIAL FROM ALL OVER',
+        description: 'One of our primary goals is to attract young and talented people worldwide to come and join us as we help create new products, services, experiences and business models that grow the UK economy. We have a diverse, open and agile culture. We work together, across boundaries, to meet the needs of our customers and to help our Company win. You will be working side by side with some of the brightest minds to make the UK a digital world leader. We will introduce you to the industry you desire and provide you with the proper training, experiences and qualifications to help you kick start your career and lead you to a path to success.',
+        imageUrl: ""
+    },
+    {
+        title: 'PROVIDE ANSWERS TO EVERY CHALLENGE',
+        description: 'We believe that a business solution is a combination of ideas used to help a company achieve its goals and objectives. Some relevant solutions include technology evaluation, the organisation of complex business information and strategic planning. Businesses generate strategies that are action-oriented to develop their market. Naturally, our strategy is aligned with what we believe in. We distinguish what our clients need and help them identify the main roadblock they are faced with and how to resolve them. Vasave offers a wide range of unique business solutions that make our clients lives more manageable, leading to taking their business to the next level. Our main goal is to provide our clients with the latest technologies to make users more efficient and effective to save time doing their tasks. We are committed to delivering the most trusted and reliable solutions tailored to your business',
+        imageUrl: ""
+    },
+  ];
+
+export const ourServices = [
+    { 
+        title: 'CLOUD',
+        description: 'It can be difficult for you to switch to a more efficient and stable type of storage in order to secure your business. Maybe its because you have never considered a different storage option. At Vasave, we provide specialized assistance and sound advice on selecting and implementing the best cloud solution for your business. We are here to help you make the switch from local storage to cloud storage a lot easier and less stressfu',
+        imageUrl: "",
+        learnMore: true,
+        suServices: [
+            {
+                title: 'MICROSOFT',
+                description: 'With services like advanced Office 365 applications, Microsoft services are built to help everyone in accessing intelligent cloud services, world-class security and much more.',
+                imageUrl: "",
+                getInTouch: true
+            },
+            {
+                title: 'GOOGLE WORKPLACE',
+                description: 'We provide Google Suite as a service for cloud computing, collaboration, and productivity. Google Workspace is an easy to use, reliable set of software and resources providing simplicity to work across all company styles and sizes. Its the perfect cloud service for collaboration and efficient work from any location and on any compatible platform, whether the company is small, medium, or a large enterprise. G Suite includes Gmail, Google Calendar, Hangouts, Google Drive, Google Docs, and a number of other services at a significantly lower cost than any other software application, as well as more benefits of operating with the software, all of which can be used to promote the brand and territories.',
+                imageUrl: "",
+                learnMore: true
+            },
+            {
+                title: 'AWS',
+                description: 'As a Cloud service provider, we provide AWS managed solutions to customers to help build a seamless transition to the cloud. Our products are delivered as modular subscription-based service that adapts to customer needs and can be deployed in cloud, on-premises, or hybrid models. We offer AWS services to a variety of sectors, including Banking, Insurance, Retail, and Health care.',
+                imageUrl: "",
+                getInTouch: true
+            },
+            {
+                title: 'HP',
+                description: 'As an expert HP cloud partner, we help determine the organizations benefits by migrating a required application to the cloud for optimal performance and cost-saving. We will help our clients create an optimal public and private infrastructure ecosystem by advising them to take a hybrid approach to their IT infrastructure. We insist on showing our clients where they can put a specific application to get the most out of it. We also provide service to set a comprehensive plan for the cloud environment',
+                imageUrl: "",
+                contactUs: true
+            },
+            {
+                title: 'CISCO UMBRELLA',
+                description: 'We provide Cisco Umbrella as a service for safe Internet connectivity and the use of cloud services everywhere.',
+                imageUrl: "",
+                getInTouch: true
+            },
+            {
+                title: 'VMWARE',
+                description: 'As VMware Cloud providers, we enable the fast and cost-effective deployment of hybrid cloud services increasingly for our clients whether they are providing infrastructure-as-a-service, crisis recovery, or desktop as a service solution. To support clients in the cloud computing industry, the service provider can take advantage of the shorter time to market, lower capital expenditures, and lower infrastructure costs.',
+                imageUrl: "",
+                contactUs: true
+            },
+        ],
+
+    },
+    {
+        title: 'CONSULTANCY',
+        description: 'Sometimes a business needs outside-the-box advice an outsiders perspective. Our consultancy services provide just that and much more. We offer quick and efficient expert advice for your business. We also provide resources which can help your company jump onto the bandwagon of future success. Get in touch with our dedicated team in just a few clicks.',
+        imageUrl: "",
+        learnMore: true,
+        subServices: [
+            { 
+                title: 'E-COMMERCE',
+                description: 'E-commerce is widely regarded as a highly advantageous form of doing business, as it offers several conveniences and time-saving methods. This, combined with the worlds rapidly rising user base, would eventually make it the preferred method of doing business.',
+                imageUrl: "",
+                contactUs: true
+            },
+            {
+                title: 'M-COMMERCE',
+                description: 'M-Commerce is a term that refers to any commercial transaction that is performed electronically through a mobile phone using wireless technologies. Our M-Commerce Services will help the our clients in supporting their users experience.',
+                imageUrl: "",
+                contactUs: true
+            },
+            {
+                title: 'RECRUITMENT',
+                description: 'As an IT service and solution provider, we provide businesses with aid in recruitment: identifying, attracting, screening, shortlisting, and interviewing suitable candidates for a job at your organisation, whether it is for a permanent or temporary role.',
+                imageUrl: "",
+                contactUs: true
+            },
+            {
+                title: 'TRAINING',
+                description: 'Our service portfolio primarily caters to students, aspiring professionals, and working professionals (retail), educational institutions, corporate institutions, and government organizations (enterprises). We offer training services and career enhancement programmes to the retail consumer market, which are organised as short, part-time skills upgrade programmes and full-time, job-oriented programmes. Corporate training, seminars, staffing solutions, and product and equipment advisory services are among the business services we provide. We also offer global certification exam services for technology leaders such as Microsoft, IBM, Amazon, HP, Oracle, and others to students and corporate clients.',
+                imageUrl: "",
+                contactUs: true
+            },
+        ],
+    },
+    {
+        title: 'CYBER SECURITY',
+        description: 'Cyber Essentials is an easy but powerful, Government-backed scheme that will help you defend your organisation, whatever its capacity, against different types of the most common cyber attacks that can potentially harm your data. Cyber attacks vary in their shapes and sizes. Still, the vast majority are fundamental, carried out by relatively unskilled individuals, and our guidance is designed to prevent these attacks from even touching your devices.',
+        imageUrl: "",
+        learnMore: true,
+        subServices: [
+            {
+                title: 'WHAT IS CYBER ESSENTIALS?',
+                description: 'Cyber Essentials is an easy but powerful, Government-backed scheme that will help you defend your organisation, whatever its capacity, against different types of the most common cyber attacks that can potentially harm your data. Cyber attacks vary in their shapes and sizes. Still, the vast majority are fundamental, carried out by relatively unskilled individuals, and our guidance is designed to prevent these attacks from even touching your devices.',
+                imageUrl: ""
+            },
+            {
+                title: 'WHAT IS CYBER ESSENTIALS PLUS?',
+                description: 'Cyber Essentials Plus involves an audit of your system by one of the trained assessors. They are involved to confirm that all controls that have been declared in Cyber Essentials are implemented within the organisations network. This is done by undertaking and completing Cyber Essentials Plus,it is your choice whether to declare publicly to your supply chain and customers that your organisation has been proven to meet Cyber Essentials baseline standards which enables you to interact with clients, business partners and staff confidently and securely.',
+                imageUrl: ""
+
+            },
+
+        
+            
+        ],
+        certificationLevels: [
+            {
+                title: 'CYBER ESSENTIALS',
+                description: 'All the Essentials to protect your business from cyber criminals and attacks today.',
+                imageUrl: "",
+                learnMore: true
+            },
+            {
+                title: 'CYBER ESSENTIALS PLUS',
+                description: 'Everything that the Cyber Essentials certification covers plus extra content to really get the most out for your organisation.',
+                imageUrl: "",
+                learnMore: true
+            },
+         ],
+         ourCertifications: [
+            {
+                title: 'CYBER ESSENTIALS',
+                imageUrl: "",
+                openPdf: true
+            },
+            {
+                title: 'IASME GOVERNANCE',
+                imageUrl: "",
+                openPdf: true
+
+            },
+
+         ],
+
+    },
+    {
+        title: 'INFRASTRUCTURE',
+        description: 'It would be a sin to overlook our extremely well thought-of infrastructure services. Our exceptional delivery of service and budget fixed costs will make our service more appealing to you and your business. Based on your unique needs, we will build an infrastructure for your business, ensuring a scale to growth strategy is formulated.',
+        imageUrl: "",
+        learnMore: true,
+        subServices: [
+            {
+                title: 'KASPERSKY',
+                description: 'Need security when browsing the internet? What about an anti virus to prevent malicious softwares penetrating your system? Kaspersky has you covered, as one of the largest and most powerful Cybersecurity softwares on the market...',
+                imageUrl: "",
+                learnMore: true
+            },
+            {
+                title: 'SOPHOS',
+                description: 'In IT Protection Systems, Sophos is one of the most respected brands. We sell and deploy a wide variety of Sophos tools for highly protected application security to match all clients’ requirements from network and end-user safety. By securing the endpoint, we will guarantee that malware stays safe for your applications and that all the data is protected. As well as support for both the computers and handheld apps, Sophos also provides confidentiality and file sharing for other users. Some of the key benefits include: Scan all data written on or write from cloud NetApp for viruses, or isolation removed tainted data. Avoids inappropriate code spread to other computers on the web. Detects viruses, spyware, and other malware. Limited receiver updates which have a minimum repercussion on machine capital.',
+                imageUrl: "",
+                contactUs: true
+            },
+            {
+                title: 'HARDWARE',
+                description: 'All organisations wish that their hardware lasts longer and can save money. We provide service on the equipment and offer preventive maintenance steps, so the company gets maximum return on the investment.',
+                imageUrl: "",
+                getInTouch: true
+            },
+            {
+                title: 'DATABASE',
+                description: 'Vasave Business Solutions provides a database as a service to clients for any job or application creation scheme. DBaaS is not only quite versatile. It has all the benefits and drawbacks similar to many cloud providers, such as more effective expense management on the one side with more limited capabilities on the other side than the on-premise option. DBaaS benefits include reduced entry thresholds, expanded exposure to technology historically only available to big businesses, and digitally native usage cases such as the Internet of Things (IoT) data sharing, machine learning (ML) instruction, and advanced systems such as edge computing adjuncts.',
+                imageUrl: "",
+                contactUs: true
+            },
+        ],
+    },
+    {
+        title: 'BIOINFORMATICS',
+        description: 'Our bioinformatics Data Analysis as a Service provides stand-alone bioinformatics analysis for NGS or microarray data to help make a biological interpretation of your results without having to pay £1000s on NGS or microarray software for a one-off project.',
+        imageUrl: "",
+        learnMore: true,
+        subServices: [
+            {
+                title: 'NEXT GENERATION SEQUENCING DATA ANALYSIS',
+                description: 'Transcriptomics (RNASeq) - BAM/FASTQ → Differential gene expression → e.g( Cancer, other diseases). Subtyping / clustering. Single cell RNASeq data analysis. TCR (T-cell receptor data analysis). Genomics - BAM/fastq → variants → annotation.',
+                imageUrl: ""
+
+            },
+            {
+                title: 'POPULATION GENETIC ANALYSIS',
+                imageUrl: "",
+
+            },
+            {
+                title: 'COMPARATIVE GENOMICS',
+                imageUrl: ""
+            },
+            {
+                title: 'MICROARRAY DATA ANALYSIS',
+                imageUrl: ""
+            },
+            {
+                title: 'DE NOVO BASED GENOME AND TRANSCRIPTOME ASSEMBLY',
+                imageUrl: ""
+
+            },
+            {
+                title: 'VISUALIZATION OF DATA USING R PACKAGES',
+                imageUrl: ""
+            },
+            {
+                title: 'PROJECT MANAGEMENT',
+                imageUrl: ""
+            },
+            {
+                title: 'HANDLE PROJECT USING AGILE PROCESS FOR SUCCESSFUL AND TIMELY DELIVERY',
+                description: 'Work with multiple stakeholders to implement project. Manage the project by Epic → Story → Tasks. GUI and backend testing. Project to development to testing to deployment. Tools like JIRA, confluence etc.',
+                imageUrl: ""
+            },
+            {
+                title: 'EXPERIENCE OF WORKING ON',
+                description: 'High Performance Computing environment, End to end Workflow/Pipeline development including pdf report generation, Technical Expertise, Linux, Perl, R, Python, Shell, mongodb, MySQL, Postgresq, AWS, Bioconductor, Docker, GitHub.',
+                imageUrl: ""
+            },
+
+        ],
+    },    
+];
